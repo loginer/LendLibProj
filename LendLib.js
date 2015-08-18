760ec8d6fae3879871f9b4c268b51b657c1a69d2
Leonid R <leonid.rubin@gmail.com>
2015-08-18 10:04:54 +0300

    Second changes

--- LendLib.js
+++ LendLib.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
- /* Template.hello.helpers({
+ /*--- Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
